09f78931f385be3cf2438000e8001c0188315bc7
neerajmehra <dhan112154>
2021-03-19 15:38:07 +0530

    Adds: Test Runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,8 +1,40 @@
 package runner;
 
-//@CucumberOptions(
-//        features = {"src/test/resources/Test.feature"}
-//        , glue = "steps"
-//)
-public class TestRunner {
+import io.cucumber.testng.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@CucumberOptions(features = "src/test/resources/Test.feature", glue = {"steps"}
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
+    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+        testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
+    }
+
+    @DataProvider
+    public Object[][] scenarios() {
+        if (testNGCucumberRunner == null) {
+            return new Object[0][0];
+        }
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() {
+        if (testNGCucumberRunner == null) {
+            return;
+        }
+        testNGCucumberRunner.finish();
+    }
 }
