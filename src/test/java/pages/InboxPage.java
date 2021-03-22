715104f50f5fc66100f92b8858f5a0ad781ad525
neerajmehra <dhan112154>
2021-03-22 21:10:07 +0530

    Adds: Index POM

---
+++ src/test/java/pages/InboxPage.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InboxPage extends BasePage {
+    private final By primaryTab = By.xpath("//*[@aria-label='Primary']");
+    private final By contactButton = By.xpath("//*[@aria-label='Contacts']");
+    private final By contactFrame = By.xpath("//iframe[contains(@title,'Contacts')]");
+    private final By contactNames = By.cssSelector(".iAmyCb.AL18ce");
+
+    public InboxPage(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+
+    public void isPrimaryTabAvailable() {
+        driver.findElement(primaryTab).isDisplayed();
+    }
+
+    public void clickContacts() {
+        driver.findElement(contactButton).click();
+    }
+
+    public List<String> getAllContactsName() {
+        WebElement contactFrameWebElement = driver.findElement(contactFrame);
+        driver.switchTo().frame(contactFrameWebElement);
+
+        List<WebElement> contactWebElements = driver.findElements(contactNames);
+        List<String> gmailContacts = new ArrayList<>();
+        for (WebElement contact : contactWebElements) {
+            gmailContacts.add(contact.getText());
+        }
+        return gmailContacts;
+    }
+}
