3a58af331b6b0ac4f2c76364de81fe7a7aa72b28
neerajmehra <dhan112154>
2021-03-22 21:35:17 +0530

    Adds: Base Step

---
+++ src/test/java/steps/BaseStep.java
@@ -0,0 +1,12 @@
+package steps;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
+public class BaseStep {
+    public static Properties properties;
+    public WebDriver driver;
+}
+
+
